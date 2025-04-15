300abf9b99188661b19a31b1a960d58029ade76b
Jordan-celton <jordan.celton@gmail.com>
2025-04-15 15:08:30 +0200

    Ajout d'une icône d'apostrophe et réorganisation du contenu dans le composant EmotionWaterBanner, mise à jour des styles associés pour améliorer la présentation.

--- mon-projet/src/components/ServicesPage/EmotionWaterBanner.jsx
+++ mon-projet/src/components/ServicesPage/EmotionWaterBanner.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "../../styles/ServicesPage/EmotionWaterBanner.css";
+import apostrophe from "../../assets/icons/double-quotes-svgrepo-com.svg";
 
 const EmotionWaterBanner = () => {
   return (
     <div className="emotion-water-banner">
-      <h1 className="banner-title">
-        Créer l’émotion par l’eau
-        <br />
-        <span className="banner-subtitle">
-          avec des fontaines durables et sur mesure
-        </span>
-      </h1>
+      <div className="banner-content">
+        <img src={apostrophe} alt="Citation" className="apostrophe-icon" />
+        <h1 className="banner-title">
+          Créer l'émotion par l'eau
+          <br />
+          <span className="banner-subtitle">
+            avec des fontaines durables et sur mesure
+          </span>
+        </h1>
+      </div>
     </div>
   );
 };
