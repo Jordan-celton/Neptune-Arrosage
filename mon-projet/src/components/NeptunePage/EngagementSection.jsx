5caff00cf5f8a39ac4295b5221f7debf96713596
Jordan-celton <jordan.celton@gmail.com>
2025-04-20 15:38:53 +0200

    feat: Ajout d'icônes et de cartes dans la section Engagement pour une meilleure présentation des services

--- mon-projet/src/components/NeptunePage/EngagementSection.jsx
+++ mon-projet/src/components/NeptunePage/EngagementSection.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import engagementImage from "../../assets/photos/Home/bg_nos_services.webp";
+import icon from "../../assets/icons/picto_bulles.svg";
+import icon1 from "../../assets/icons/picto_materiel_arrosage.svg";
+import icon2 from "../../assets/icons/picto_parc_jardins.svg";
+import icon3 from "../../assets/icons/picto_recup_EP.svg";
+
+import cardsImage1 from "../../assets/photos/Home/A_photograph_captures_a_Wallace_Fountain_situated_.webp";
+import cardsImage2 from "../../assets/photos/Home/header_accueil.webp";
+import cardsImage3 from "../../assets/photos/Home/bg_nos_services.webp";
+
 import "../../styles/NeptunePage/EngagementSection.css";
 
 const EngagementSection = () => {
   return (
     <section className="engagement-section">
       <div className="engagement-container">
-        {/* Colonne image */}
-        <div className="engagement-image-container">
-          <img
-            src={engagementImage}
-            alt="Solutions durables de gestion de l'eau"
-            className="engagement-image"
-          />
-          <div className="image-overlay"></div>
-        </div>
-
         {/* Colonne contenu */}
         <div className="engagement-content">
           <div className="engagement-header">
-            <span className="engagement-number">8°</span>
+            <img src={icon} className="engagement-icon" alt="Engagement icon" />
             <h1 className="engagement-title">Un fort engagement</h1>
           </div>
 
           <h2 className="engagement-subtitle">
-            Pour une gestion raisonnée de l'eau
+            Pour une gestion <span>raisonnée de l'eau</span>
           </h2>
 
           <div className="engagement-text">
@@ -34,22 +33,53 @@ const EngagementSection = () => {
               l'eau. L'entreprise développe des solutions innovantes pour
               optimiser l'usage des ressources hydriques, notamment à travers :
             </p>
-
-            <ul className="engagement-list">
-              <li>
-                <strong>Pompage des eaux de surface</strong> pour limiter
-                l'utilisation d'eau potable
-              </li>
-              <li>
-                <strong>Stockage des eaux de pluie</strong> pour réutilisation
-                optimisée
-              </li>
-              <li>
-                <strong>Réutilisation des eaux grises</strong>, une alternative
-                efficace et écologique
-              </li>
-            </ul>
           </div>
+        </div>
+
+        {/* Colonne image */}
+        <div className="engagement-image-container">
+          <img
+            src={engagementImage}
+            alt="Solutions durables de gestion de l'eau"
+            className="engagement-image"
+          />
+          <div className="image-overlay"></div>
+        </div>
+      </div>
+
+      {/* Cartes superposées */}
+      <div className="engagement-card-list">
+        <div className="engagement-card">
+          <img src={cardsImage1} alt="Pompage des eaux" />
+          <div className="engagement-card-icon">
+            <img src={icon1} alt="Pompage icon" />
+          </div>
+          <p>
+            <strong>Pompage des eaux de surface</strong> pour limiter
+            l'utilisation d’eau potable
+          </p>
+        </div>
+
+        <div className="engagement-card">
+          <img src={cardsImage2} alt="Stockage des eaux" />
+          <div className="engagement-card-icon">
+            <img src={icon2} alt="Stockage icon" />
+          </div>
+          <p>
+            <strong>Stockage des eaux de pluie</strong> pour réutilisation
+            optimisée
+          </p>
+        </div>
+
+        <div className="engagement-card">
+          <img src={cardsImage3} alt="Réutilisation des eaux grises" />
+          <div className="engagement-card-icon">
+            <img src={icon3} alt="Réutilisation icon" />
+          </div>
+          <p>
+            <strong>La réutilisation des eaux grises</strong>, une alternative
+            efficace et écologique
+          </p>
         </div>
       </div>
     </section>
