1fc5f28de34d4e1aa11e0265dd96b473c689823c
Jordan-celton <jordan.celton@gmail.com>
2025-05-31 11:27:02 +0200

    style: Ajouter Google Tag Manager et améliorer le SEO avec des balises appropriées

--- mon-projet/src/App.jsx
+++ mon-projet/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -5,26 +6,45 @@ import {
   useLocation,
 } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+
 import Home from "./pages/Home";
 import Neptune from "./pages/Neptune";
 import Services from "./pages/Services";
 import Realisations from "./pages/Realisations";
 import Contact from "./pages/Contact";
 import Recrutement from "./pages/Recrutement";
-import NotFound from "./pages/NotFound";
-import Footer from "./components/Footer";
-import React, { useEffect } from "react";
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-import ScrollToTop from "./components/ScrollToTop";
 import MentionsLegales from "./pages/MentionLegales";
+import NotFound from "./pages/NotFound";
 
-function App() {
+const GTM_ID = "GTM-5VNCT43T"; // Remplace par ton ID GTM réel
+
+function loadGTM(id) {
+  if (!window.dataLayer) {
+    window.dataLayer = [];
+  }
+  window.dataLayer.push({
+    "gtm.start": new Date().getTime(),
+    event: "gtm.js",
+  });
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
+  document.head.appendChild(script);
+}
+
+function AppContent() {
   const location = useLocation();
 
   useEffect(() => {
+    // Initialisation AOS
     AOS.init({
       duration: 1000,
       once: true,
@@ -33,11 +53,28 @@ function App() {
   }, []);
 
   useEffect(() => {
+    // Refresh AOS à chaque changement de page
     AOS.refresh();
   }, [location.pathname]);
 
+  useEffect(() => {
+    // Injecter Google Tag Manager une fois
+    loadGTM(GTM_ID);
+  }, []);
+
   return (
-    <HelmetProvider>
+    <>
+      {/* NoScript GTM (recommandé pour suivi si JS désactivé) */}
+      <noscript>
+        <iframe
+          title="GTM"
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -51,15 +88,19 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <HelmetProvider>
+      <Router>
+        <ScrollToTop />
+        <AppContent />
+      </Router>
     </HelmetProvider>
   );
 }
 
-const WrappedApp = () => (
-  <Router>
-    <ScrollToTop />
-    <App />
-  </Router>
-);
-
-export default WrappedApp;
+export default App;
